4302723dbf9313037fb4e650b314c4e5a1832552
João Mangueira <jamjs7@gmail.com>
2020-11-11 22:40:38 -0300

    Finalizando o projeto

---
+++ README.md
@@ -0,0 +1,53 @@
+<img alt="GoStack" src=".github/gostack.png" />
+
+<p align="center">
+  <a href="#rocket-tecnologias">Tecnologias</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-projeto">Projeto</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-como-rodar">Como rodar</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-como-contribuir">Como contribuir</a>&nbsp;&nbsp;&nbsp;
+  </p>
+
+<br>
+
+<p align="center">
+  <img alt="gostack" width="600px" height="600px" src=".github/app1.PNG" />
+</p>
+
+<p align="center">
+  <img alt="gostack" width="600px" height="600px" src=".github/app2.PNG" />
+</p>
+
+## GoStack - Projeto Github Explorer
+
+## 🚀 Tecnologias
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- [React](https://pt-br.reactjs.org/docs/getting-started.html)
+- [Yarn](https://yarnpkg.com/) - 1.22.4
+- [Npm](https://www.npmjs.com/) - 6.14.5
+
+
+## 💻 Projeto
+
+Projeto React que lista repositórios e mostra a Issues desse repositório.
+
+## 🚀 Como Rodar
+
+- Clone o projeto.
+- Entre na pasta do projeto e rode yarn install (pode usar npm install de acordo com a sua configuração).
+- yarn test para rodar os testes.
+- yarn start (localhost:3000).
+
+## 🤔 Como contribuir
+
+- Faça um fork desse repositório;
+- Cria uma branch com a sua feature: `git checkout -b minha-feature`;
+- Faça commit das suas alterações: `git commit -m 'feat: Minha nova feature'`;
+- Faça push para a sua branch: `git push origin minha-feature`.
+
+Depois que o merge da sua pull request for feito, você pode deletar a sua branch.
+
+## 📝 Licença
+
+Esse projeto está sob a licença MIT.
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,55 +1,88 @@
-import React from 'react';
+import React, { useState, useEffect, FormEvent } from 'react';
+import { Link } from 'react-router-dom';
 import { FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
 
 import LogoImg from '../../assets/Logo.svg';
 
-import { Title, Form, Repositories } from './style';
+import {
+  Title,
+  Form,
+  Repositories,
+  Error,
+} from './style';
 
-const Dashboard: React.FC = () => (
-  <>
-    <img src={LogoImg} alt="Gitub Explorer" />
-    <Title>Explore repositórios no Github</Title>
-    <Form>
-      <input placeholder="Digite o nome do repositório" />
-      <button type="submit">Pesquisar</button>
-    </Form>
-    <Repositories>
-      <a href="teste">
-        <img
-          src="https://avatars1.githubusercontent.com/u/1902749?s=460&u=382acdf7478c6499d9b4321e68523d17b699e802&v=4"
-          alt="João Mangueira"
-        />
-        <div>
-          <strong>jamangueira7</strong>
-          <p>fafjasdlfjasdlfja sdlfjflajfali sjfliafjlasdfjlfjlias djfliasdlifjalfjdsali</p>
-        </div>
-        <FiChevronRight size={20} />
-      </a>
-      <a href="teste">
-        <img
-          src="https://avatars1.githubusercontent.com/u/1902749?s=460&u=382acdf7478c6499d9b4321e68523d17b699e802&v=4"
-          alt="João Mangueira"
-        />
-        <div>
-          <strong>jamangueira7</strong>
-          <p>fafjasdlfjasdlfja sdlfjflajfali sjfliafjlasdfjlfjlias djfliasdlifjalfjdsali</p>
-        </div>
-        <FiChevronRight size={20} />
-      </a>
-      <a href="teste">
-        <img
-          src="https://avatars1.githubusercontent.com/u/1902749?s=460&u=382acdf7478c6499d9b4321e68523d17b699e802&v=4"
-          alt="João Mangueira"
-        />
-        <div>
-          <strong>jamangueira7</strong>
-          <p>fafjasdlfjasdlfja sdlfjflajfali sjfliafjlasdfjlfjlias djfliasdlifjalfjdsali</p>
-        </div>
-        <FiChevronRight size={20} />
-      </a>
-    </Repositories>
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  }
+}
 
-  </>
-);
+const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [inputErro, setinputErro] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>(() => {
+    const storagedRepositories = localStorage.getItem('@GithubExplore:repositories');
+    if (storagedRepositories) {
+      return JSON.parse(storagedRepositories);
+    }
+    return [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem('@GithubExplore:repositories', JSON.stringify(repositories));
+  }, [repositories]);
+
+  async function handleAddRepository(event: FormEvent<HTMLFormElement>): Promise<void> {
+    event.preventDefault();
+    if (!newRepo) {
+      setinputErro('Digite o autor/nome do repositorio');
+      return;
+    }
+    try {
+      const response = await api.get<Repository>(`repos/${newRepo}`);
+      const repository = response.data;
+      setRepositories([...repositories, repository]);
+      setNewRepo('');
+      setinputErro('');
+    } catch (err) {
+      setinputErro('Erro na busca por esse repositorio.');
+    }
+  }
+
+  return (
+    <>
+      <img src={LogoImg} alt="Gitub Explorer" />
+      <Title>Explore repositórios no Github</Title>
+      <Form hasError={!!inputErro} onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={(e) => setNewRepo(e.target.value)}
+          placeholder="Digite o nome do repositório"
+        />
+        <button type="submit">Pesquisar</button>
+      </Form>
+      { inputErro && <Error>{inputErro}</Error>}
+      <Repositories>
+        {repositories.map((repository) => (
+          <Link key={repository.full_name} to={`/repository/${repository.full_name}`}>
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+            <FiChevronRight size={20} />
+          </Link>
+        ))}
+      </Repositories>
+    </>
+  );
+};
 
 export default Dashboard;
